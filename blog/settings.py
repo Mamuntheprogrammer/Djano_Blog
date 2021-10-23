7493bc98f0e468babbfab1cdc80f4b25c897ad78
Mamuntheprogrammer <a.a.mamunbu@gmail.com>
2021-10-23 23:13:10 +0600

    share mail is working

--- blog/settings.py
+++ blog/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_FROM_EMAIL = 'ruba.bd2019@gmail.com'
+SERVER_EMAIL = 'ruba.bd2019@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER= 'ruba.bd2019@gmail.com'
+EMAIL_HOST_PASSWORD = 'iqbal@2019'
+EMAIL_USE_TLS = True
--- pygems/urls.py
+++ pygems/urls.py
@@ -6,5 +6,6 @@ app_name = 'pygems'
 urlpatterns = [
 #post view
 path('',views.post_list,name='post_list'),
-path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_details,name='post_details')
+path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_details,name='post_details'),
+path('<int:post_id>/share/',views.post_share,name='post_share')
 ]
